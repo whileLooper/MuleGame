b05d70749c28759df4d2c049198968ff68e4e054
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-10-25 01:28:27 +0000

    Basic Class

---
+++ M.U.L.E./src/Drive.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+public class Drive extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Drive(){
+		
+	}
+}
---
+++ M.U.L.E./src/Town.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Town extends JPanel implements KeyListener {
+	private imageIcon backGround = (new ImageIcon("src/townBackground.png")).getImage();
+	private Game game;
+	private Point pLocation;
+	private Player player;
+	
+	public Town(Player p, Game g) {
+		game = g;
+		player = p;
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.drawImage(backGround, 0, 0, 1000, 1000, null); // Draws background
+		g.drawImage(p.getImageIcon(), 0, 0, 50, 50, null); // Draws player
+	}
+	
+	public void keyPressed(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {
+		if (e.getKeyChar() == 'w' && pLocation.getY() > 0) {
+			pLocation.setLocation(pLocation.getX(), pLocation.getY()-1);
+		}
+		else if (e.getKeyChar() == 's' && pLocation.getY() < backGround.getHeight()) {
+			pLocation.setLocation(pLocation.getX(), pLocation.getY()+1);
+		}
+		else if (e.getKeyChar() == 'd' && pLocation.getX() < backGround.getWidth()) {
+			pLocation.setLocation(pLocation.getX()+1, pLocation.getY());
+		}
+		else if (e.getKeyChar() == 'a' && pLocation.getX() > 0) {
+			pLocation.setLocation(pLocation.getX()-1, pLocation.getY());
+		}
+		if (pLocation.getY() == backGround.getHeight()) {
+			if (pLocation.getX() > 100 && pLocation.getX() < 100) {
+				game.playerOutTown(); // Player get back to Map
+			}
+		}
+		repaint();
+}
