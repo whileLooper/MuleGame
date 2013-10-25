f8adece8b005ececd2a73cbb767076b5a92965d0
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-10-25 02:29:02 +0000

    Map Class

--- M.U.L.E./src/Map.java
+++ M.U.L.E./src/Map.java
@@ -1,4 +1,58 @@
+import java.awt.Dimension;
+import java.awt.Point;
 
-public class Map {
+import javax.swing.JPanel;
 
+
+public class Map extends JPanel{
+
+	
+	public Map(String mapType){
+		createMap(mapType);
+		setPreferredSize(new Dimension(900, 500));
+		setVisible(true);
+		validate();
+	}
+	
+	/**
+	 * This method decides which type of the map to create, and call the responding method
+	 * @param mapType is the type of the map
+	 */
+	private void createMap(String mapType){
+		switch(mapType){
+		case "Standard":{
+			standardMap();
+			break;
+		}
+		case "Ramdon":{
+			randomMap();
+			break;
+		}
+		}
+	}
+	
+	/**
+	 * This method initiates a standard map
+	 */
+	private void standardMap(){
+		String[][] cmap = { {"P","P","M1","P","R","P","M3","P","P"},
+				            {"P","M1","P","P","R","P","P","P","M3"},
+				            {"M3","P","P","P","Town","P","P","P","M1"},
+				            {"P","M2","P","P","R","P","M2","P","P"},
+				            {"P","P","M2","P","R","P","P","P","M2"}};		
+
+		for(int i = 0; i < 5; i ++){
+			for(int j = 0; j < 9; j++){
+				Tile tile = new Tile(cmap[i][j]);
+				add(tile);
+			}
+		}
+	}
+	
+	/**
+	 * This method initiates a random map
+	 */
+	private void randomMap(){
+		
+	}
 }
