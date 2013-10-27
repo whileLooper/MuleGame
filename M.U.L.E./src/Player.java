8704f38c21cfc9e42b180d823756265cf1586ac2
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-10-27 23:19:07 +0000

    Player can buy land

--- M.U.L.E./src/Player.java
+++ M.U.L.E./src/Player.java
@@ -14,6 +14,11 @@ public class Player {
 	private Image image;
 	private ArrayList<Tile> lands;
 	
+	private int money;
+	private int energy;
+	private int ore;
+	private int crystal;
+	
 	/**
 	 * This is the Player class' constructor
 	 * @param n is the player's name
@@ -34,4 +39,20 @@ public class Player {
 		return color;
 	}
 
+	/**
+	 * This method is for players to buy lands, first it checks whether player have enough money, and check whether the land 
+	 * is purchasable, if so, then the deal is done.
+	 * @param price is the price for the tile
+	 * @param land is the tile to buy
+	 */
+	public void buyLand(int price, Tile land){
+		if(money > price){
+			if(land.beBought(this)){
+				money -= price;
+				lands.add(land);
+			}
+		}else{
+			System.out.println("Oops, you don't have enought money...");
+		}
+	}
 }
