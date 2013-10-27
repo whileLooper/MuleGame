1e625355aad10fec37b9328b09aa010ad787fa54
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-10-27 23:03:03 +0000

    Player class update

--- M.U.L.E./src/Player.java
+++ M.U.L.E./src/Player.java
@@ -1,11 +1,37 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Image;
+import java.util.ArrayList;
+
 
 public class Player {
-
-	private Image image = new ImageIcon("src/player.png").getImage();
 	
-	public Image getImage() {
-		return image;
+	/**
+	 * This is the Player class, used to store player's information
+	 */
+	private String name;
+	private Color color;
+	private String race;
+	private Image image;
+	private ArrayList<Tile> lands;
+	
+	/**
+	 * This is the Player class' constructor
+	 * @param n is the player's name
+	 * @param c is the player's color
+	 * @param r is the player's race
+	 */
+	public Player(String n, Color c, String r){
+		name = n;
+		color = c;
+		race = r;
 	}
+	
+	/**
+	 * This method returns player's color 
+	 * @return player's color
+	 */
+	public Color getColor(){
+		return color;
+	}
+
 }
