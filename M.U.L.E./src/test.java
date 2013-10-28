f73bee44fb82fd6a1d8e6500bb20f5152f412f2a
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-10-28 16:31:11 +0000

    Player update

--- M.U.L.E./src/test.java
+++ M.U.L.E./src/test.java
@@ -8,9 +8,8 @@ public class test {
 		// TODO Auto-generated method stub
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		Map map = new Map("Standard");
-	    frame.getContentPane().add(map);
-	    frame.add(new Start());
+		
+	    frame.add(new Start(new Game(new Drive())));
 	    frame.setSize(905,528);
 	    frame.setVisible(true);
 	    frame.setResizable(false);
