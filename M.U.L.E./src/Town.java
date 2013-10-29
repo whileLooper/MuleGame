7b27d09f02201fbfaf4c8a9990c1081934591439
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-10-29 01:54:31 +0000

    Town player enter town

--- M.U.L.E./src/Town.java
+++ M.U.L.E./src/Town.java
@@ -1,96 +1,76 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
-import StorePackage.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-/**
- * 
- * @author oneE
- *
- */
-public class Town extends JPanel implements KeyListener {
-	/**
-	 * backGround = background image
-	 * pLocation = location of player
-	 */
-	private Image backGround;
+
+public class Town extends JPanel{
+	
 	private Game game;
-	private Point pLocation;
-	private Player player;
-	private Store pub;
-	private final int PLAYER_SIZE = 50;
 	
 	/**
-	 * Constructor for town.
-	 * 
-	 * @param p Player
-	 * @param g Game
-	 * @param direction Direction of player when entering town.
+	 * This is the constructor for Town class
+	 * @param g is the game
 	 */
-	public Town(Player p, Game g, String d) {
+	public Town(Game g){
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				Player player= game.getCurrentPlayer();
+				Point p = player.getTownLocation();
+				int x = p.x;
+				int y = p.y;
+				int key = e.getKeyCode();
+				switch(key){
+				case KeyEvent.VK_DOWN:{
+					y += 10;
+					break;
+				}
+				case KeyEvent.VK_UP:{
+					y -= 10;
+					break;
+				}
+				case KeyEvent.VK_RIGHT:{
+					x += 10;
+					break;
+				}
+				case KeyEvent.VK_LEFT:{
+					x -= 10;
+					break;
+				}
+				default:
+				}
+				
+				if(x + 50 >= 500 || x <= 0 || y + 50 >= 300 && y <= 0){
+					System.out.println("leave town");
+					game.playerLeaveTown();
+				}else if((x > 0 && x + 50 < 500 && y >= 100 && y + 50 <= 200) || (x >= 200 && x + 50 <= 300 && y > 0 && y + 50 < 300)){
+					player.setTownLocation(new Point(x, y));
+					repaint();
+				}else{
+					
+				}
+			}
+		});
 		game = g;
-		player = p;
-		backGround = new ImageIcon("src/townBackground.png").getImage();
-		// d = Starting location
-		switch(d) {
-			case "NORTH": pLocation=new Point(200+PLAYER_SIZE/2, 0);
-			case "SOUTH": pLocation=new Point(200+PLAYER_SIZE/2, backGround.getHeight(null)-PLAYER_SIZE);
-			case "EAST": pLocation=new Point(0,100+PLAYER_SIZE/2);
-			case "WEST": pLocation=new Point(backGround.getWidth(null)-PLAYER_SIZE, 100+PLAYER_SIZE/2);
-		}
-		pub = new Pub();
-	}
-	
-	/**
-	 * The draw method for town.
-	 */
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		g.drawImage(backGround, 0, 0, 1000, 1000, null); // Draws background
-		/**
-		 * Player.getImage() method missing.
-		 */
-	//	g.drawImage(player.getImage(), (int)pLocation.getX(), (int)pLocation.getY(), PLAYER_SIZE, PLAYER_SIZE, null); // Draws player
-	}
-	
-	public void keyPressed(KeyEvent e) {}
-	public void keyReleased(KeyEvent e) {}
-	public void keyTyped(KeyEvent e) {
-		if (e.getKeyChar() == KeyEvent.VK_UP && pLocation.getY() > 0) {
-			pLocation.setLocation(pLocation.getX(), pLocation.getY()-1);
-		}
-		else if (e.getKeyChar() == KeyEvent.VK_DOWN && pLocation.getY() < backGround.getHeight(null)) {
-			pLocation.setLocation(pLocation.getX(), pLocation.getY()+1);
-		}
-		else if (e.getKeyChar() == KeyEvent.VK_RIGHT && pLocation.getX() < backGround.getWidth(null)) {
-			pLocation.setLocation(pLocation.getX()+1, pLocation.getY());
-		}
-		else if (e.getKeyChar() == KeyEvent.VK_LEFT && pLocation.getX() > 0) {
-			pLocation.setLocation(pLocation.getX()-1, pLocation.getY());
-		}
-		if (inBorder(pLocation, pub.getArrayListBorder())) {
-			/**
-			 * Player enter pub method missing.
-			 */
-		}
+		setFocusable(true);
+		setPreferredSize(new Dimension(500, 300));
+		setVisible(true);
 		repaint();
 	}
+	
+	@Override
 	/**
-	 * Check if Point x is in border
-	 * @param x: current location of player
-	 * @param p: arraylist of border
-	 * @return true if in border, false if not in.
+	 * This method overrides its superclass'
 	 */
-	public boolean inBorder(Point x, ArrayList<Point> p) {
-		for (int i = 0; i < p.size(); i++) {
-			if (x.equals(p.get(i))) {
-				return true;
-			}
-		}
-		return false;
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawImage((new ImageIcon("townp.png")).getImage(), 0,0, 500, 300, null);
+		game.getCurrentPlayer().drawOnTown(g);
 	}
 }
