5493f70b1f3da74e12dd479f15fc3066e0d7bd70
yang.shen@dhs.sg <yang.shen@dhs.sg@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-11-01 00:19:44 +0000

    fixed store

--- M.U.L.E./src/Store.java
+++ M.U.L.E./src/Store.java
@@ -20,8 +20,8 @@ public class Store {
 	 * Constructor for Store Class
 	 */
 	
-	public Store() {
-		if (Game.getDifficulty() == "Beginner") {
+	public Store(String difficulty) {
+		if (difficulty == "Beginner") {
 			food = 16;
 			energy = 16;
 			smithore = 0;
@@ -36,73 +36,45 @@ public class Store {
 			mule = 14;
 		}
 	}
-	
-	public static boolean checkSufficient(Player p, int n) {
-		if (p.getMoney() > n) {
+		
+	public boolean buy(String type) {
+		if (type == "food" && food>0 && mule>0) {
+			mule-=1;
+			food-=1;
+			return true;
+		}
+		else if (type == "energy" && energy>0 && mule>0) {
+			mule-=1;
+			energy-=1;
+			return true;
+		}
+		else if (type == "smithore" && smithore > 0 && mule > 0) {
+			mule-=1;
+			smithore-=1;
+			return true;
+		}
+		else if (type == "crystite" && crystite > 0 && mule > 0) {
+			mule-=1;
+			crystite-=1;
 			return true;
 		}
 		return false;
 	}
 	
-	public static void deductFunds(Player p, int n) {
-		p.deductMoney(-n);
-	}
-	
-	public int buy(String type, Player p) {
-		if (type == "foodM" && p.getMoney() > FOODM_PRICE && food>0 && mule>0 && p.getMule() == 0) {
-			p.addFood();
-			p.addMule();
-			p.deductMoney(FOODM_PRICE);
-			mule-=1;
-			food-=1;
-		}
-		else if (type == "energym" && p.getMoney() > ENERGYM_PRICE && energy>0 && mule>0 && p.getMule() == 0) {
-			p.addEnergy();
-			p.addMule();
-			p.deductMoney(ENERGYM_PRICE);
-			mule-=1;
-			energy-=1;
-		}
-		else if (type == "smithorem" && p.getMoney() > SMITHOREM_PRICE && smithore > 0 && mule > 0 && p.getMule() == 0) {
-			p.addSmithore();
-			p.addMule();
-			p.deductMoney(SMITHOREM_PRICE);
-			mule-=1;
-			smithore-=1;
-		}
-		else if (type == "crystitem" && p.getMoney() > CRYSTITEM_PRICE && crystite > 0 && mule > 0 && p.getMule() == 0) {
-			p.addCrystite();
-			p.addMule();
-			p.deductMoney(CRYSTITEM_PRICE);
-			mule0=1;
-			crystite-=1;
-		}
-		return p.getMoney();
-	}
-	
-	public int sell(String type, Player p) {
-		if (type == "food" && p.getFood() > 0) {
-			p.deductFood();
-			p.addMoney(FOOD_PRICE);
+	public void sell(String type) {
+		if (type == "food") {
 			food+=1;
 		}
-		else if (type == "energy" && p.getEnergy() > 0) {
-			p.deductEnergy();
-			p.addMoney(ENERGY_PRICE);
+		else if (type == "energy") {
 			energy+=1;
 		}
-		else if (type == "smithore" && p.getSmithore() > 0) {
-			p.deductSmithore();
-			p.addMoney(SMITHORE_PRICE);
+		else if (type == "smithore") {
 			smithore+=1;
 			mule+=1;
 		}
-		else if (type == "crystite" && p.getCrystite() > 0) {
-			p.deductCrystite();
-			p.addMoney(CRYSTITE_PRICE);
+		else if (type == "crystite") {
 			crystite+=1;
 		}
-		return p.getMoney();
 	}
 	
 	public int getFoodPrice() {
@@ -121,5 +93,17 @@ public class Store {
 		return CRYSTITE_PRICE;
 	}
 	
+	public int getFoodMulePrice() {
+		return FOODM_PRICE;
+	}
 	
+	public int getEnergyMulePrice() {
+		return ENERGYM_PRICE;
+	}
+	public int getSmithoreMulePrice() {
+		return SMITHOREM_PRICE;
+	}
+	public int getCrystieMulePrice() {
+		return CRYSTITEM_PRICE; 
+	}
 }
