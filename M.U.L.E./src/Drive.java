1a1d5bec5a32c77806220fbccd6e8ef06f1049bd
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-11-12 05:39:05 +0000

    Drive

--- M.U.L.E./src/Drive.java
+++ M.U.L.E./src/Drive.java
@@ -1,6 +1,14 @@
 import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 import javax.swing.JFrame;
+
+import com.google.gson.Gson;
 
 
 @SuppressWarnings("serial")
@@ -9,22 +17,35 @@ public class Drive extends JFrame{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Drive drive = new Drive();
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new BorderLayout());
-		Game game = new Game(frame);
-	    frame.setSize(910,688);
-	    frame.setVisible(true);
-	    frame.setResizable(false);
+		Drive drive = new Drive();
+	    
 	}
 	
 	private int x = 910;
 	private int y = 688;
+	private Game game;
 	
 	public Drive(){
 		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		setSize(x,y);
-		setVisible(true);
-		setResizable(false);
+		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		game = new Game(this);
+		setSize(910,688);
+	    setVisible(true);
+	    setResizable(false);
+	    addWindowListener(new WindowClosed());
+	}
+	
+	
+	private class WindowClosed extends WindowAdapter{
+		@Override
+		public void windowClosing(WindowEvent e){
+			Gson gson = new Gson();
+			InfoStore store = game.stop();
+			String tostore = gson.toJson(store);
+			System.out.println(game == null);
+			System.out.println(tostore);
+			store = gson.fromJson(tostore, InfoStore.class);
+		}
 	}
 }
