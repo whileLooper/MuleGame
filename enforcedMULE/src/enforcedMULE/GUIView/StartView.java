8f6f852409707d841112744a63bc105c10b1c5df
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-12-03 02:42:11 +0000

    Start View

--- enforcedMULE/src/enforcedMULE/GUIView/StartView.java
+++ enforcedMULE/src/enforcedMULE/GUIView/StartView.java
@@ -1,5 +1,40 @@
 package enforcedMULE.GUIView;
 
-public class StartView {
+import java.awt.event.MouseListener;
+
+import enforcedMULE.GUIModel.PlayerModel;
+import enforcedMULE.GUIPresenter.StartInterface;
+
+public class StartView implements StartInterface{
+
+	@Override
+	public void newGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String[] getGameInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void newPlayer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PlayerModel getPlayerInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addListener(MouseListener mouselistener) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
