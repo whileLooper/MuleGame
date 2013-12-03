f85146be4d720833434ae1ef1899d7658ed91e77
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-12-03 03:27:04 +0000

    Start model

--- enforcedMULE/src/enforcedMULE/GUIModel/StartModel.java
+++ enforcedMULE/src/enforcedMULE/GUIModel/StartModel.java
@@ -1,5 +1,33 @@
 package enforcedMULE.GUIModel;
 
+import enforcedMULE.GUIPresenter.PlayerPresenter;
+
 public class StartModel {
+	
+	public static String difficulty;
+	public static String maptype;
+	public static int numOfPlayer;
+	
+	public static PlayerPresenter[] players;
+	
+	public static int currentN = 0;
+	
+	private StartModel(){
+		
+	}
+	
+	public void setupGameInfo(String[] info){
+		difficulty = info[0];
+		maptype = info[1];
+		numOfPlayer = Integer.parseInt(info[2]);
+		players = new PlayerPresenter[numOfPlayer];
+	}
+	
+	public int addPlayer(PlayerPresenter player){
+		players[currentN] = player;
+		currentN ++;
+		if(currentN == numOfPlayer) return -1;
+		return currentN;
+	}
 
 }
