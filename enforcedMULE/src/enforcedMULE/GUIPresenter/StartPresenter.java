0e53e02474de5bc26b4fbf931370d680a9e3d91c
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-12-03 04:14:34 +0000

    start presenter

--- enforcedMULE/src/enforcedMULE/GUIPresenter/StartPresenter.java
+++ enforcedMULE/src/enforcedMULE/GUIPresenter/StartPresenter.java
@@ -1,5 +1,8 @@
 package enforcedMULE.GUIPresenter;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
 import enforcedMULE.GUIModel.StartModel;
 
 public class StartPresenter {
@@ -10,6 +13,60 @@ public class StartPresenter {
 	public StartPresenter(StartModel sM, StartInterface sI){
 		model = sM;
 		view = sI;
+		view.start();
+		addStartListener();
+	}
+
+	private void addStartListener() {
+		// TODO Auto-generated method stub
+		view.addListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JButton clicked = (JButton) e.getSource();
+				String text = clicked.getText();
+				if(text.equals("New Game")){
+					view.newGame();
+					addGameListener();
+				}else{
+					//load last game
+				}
+			}
+		});
+	}
+
+	protected void addGameListener() {
+		// TODO Auto-generated method stub
+		view.addListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JButton clicked = (JButton) e.getSource();
+				String text = clicked.getText();
+				if(text.equals("Next")){
+					model.setupGameInfo(view.getGameInfo());
+					view.newPlayerMenu(0);
+					addPlayerListener();
+				}
+			}
+		});
+	}
+
+	protected void addPlayerListener() {
+		// TODO Auto-generated method stub
+		view.addListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JButton clicked = (JButton) e.getSource();
+				String text = clicked.getText();
+				if(text.equals("Next")){
+					int n = model.addPlayer(view.getPlayerInfo());
+					if(n > -1){
+						view.newPlayerMenu(n);
+					}else{
+						//enter game
+					}
+				}
+			}
+		});
 	}
 
 }
