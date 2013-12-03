435f6e38645d2ac3a7786fa8451c548ced7eb782
shaoqian40337@gmail.com <shaoqian40337@gmail.com@cfccf51c-88aa-68f5-2d15-2852170687a2>
2013-12-03 02:41:23 +0000

    Start interface

--- enforcedMULE/src/enforcedMULE/GUIPresenter/StartInterface.java
+++ enforcedMULE/src/enforcedMULE/GUIPresenter/StartInterface.java
@@ -1,5 +1,18 @@
 package enforcedMULE.GUIPresenter;
 
+import java.awt.event.MouseListener;
+import enforcedMULE.GUIModel.PlayerModel;
+
 public interface StartInterface {
+	
+	public void newGame();
+	
+	public String[] getGameInfo();
+	
+	public void newPlayer();
+	
+	public PlayerModel getPlayerInfo();
+	
+	public void addListener(MouseListener mouselistener);
 	
 }
